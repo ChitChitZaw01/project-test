7088cb2082a1ae0f54919b34f1fd121fcd6619e6
ChitChitZaw01 <chitchitzaw.ccz16@gmail.com>
2025-03-18 14:07:24 +0700

    Init

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { AuthModule } from './auth/auth.module';
+import { User } from './user/entities/user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    password: '@Admin123',
+    username: 'postgres',
+    entities: [User],
+    database: 'pgwithnest',
+    synchronize: true,
+    logging: true,
+  }),
+  UserModule,
+  // AuthModule,
+],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
