0db3ea292efb39ead9c4872dd5a8517f3fe47b72
ChitChitZaw01 <chitchitzaw.ccz16@gmail.com>
2025-03-19 10:56:01 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   // Enable CORS
+   app.enableCors({
+    origin: 'http://localhost:4200', // Allow Angular frontend to access the backend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // Allowed headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
